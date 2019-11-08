b0a9fcbdf6d4b08bc6e4d500e859c05cabd77c51
BiggsBottor <jonatan.lledo@gmail.com>
2019-11-08 13:40:24 +0100

    typesript FASE 4 Añadidos botones de control

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,15 +1,20 @@
 "use strict";
-/*-- Default Rockets --*/
-/* //Roquet 1// */
-var rId1 = "32WESSDS";
-var rEngine1 = 3;
-var rMaxPowers1 = [10, 30, 80];
-var r1 = createRocket(rId1, rEngine1, rMaxPowers1);
-/* //Roquet 2// */
-var rId2 = "LDSFJA32";
-var rEngine2 = 6;
-var rMaxPowers2 = [30, 40, 50, 50, 30, 10];
-var r2 = createRocket(rId2, rEngine2, rMaxPowers2);
+var defaultRockets = [
+    {
+        id: "32WESSDS",
+        engines: 3,
+        maxPowers: [10, 30, 80]
+    },
+    {
+        id: "LDSFJA32",
+        engines: 6,
+        maxPowers: [30, 40, 50, 50, 30, 10]
+    }
+];
+/* //Rocket 1// */
+var r1;
+/* //Rocket 2// */
+var r2;
 /*-- Comprueba si la Id del cohete cumple el patrón --*/
 function idValidator(value) {
     var idFormat = /[A-Z0-9]{8}/; //comprueba que sean 8 carácteres alfanuméricos 
@@ -57,7 +62,7 @@ function showSpeed(rocket, sectionId) {
         rocketSpeed += engine.engineAtualSpeed;
     }
     var HTMLSpeed = "<p><b>" + rocket.id + "</b> Rocket speed <i>" + rocketSpeed + "</i><p>";
-    section.innerHTML += HTMLSpeed;
+    section.innerHTML = HTMLSpeed;
 }
 /*-- llama a la función acelerar del cohete las veces que se indique --*/
 function rocketAcceleration(rocket, times) {
@@ -71,29 +76,207 @@ function rocketDeceleration(rocket, times) {
         rocket.decelerate();
     }
 }
-/*-- FIXME: pasos por pantalla --*/
-/* Mostrar a pantalla el codi dels coets, el número de propulsors
-    que té i la potència màxima de cada propulsor.*/
-showRocket(r1, "rockets");
-showRocket(r2, "rockets");
-/* Mostrar a pantalla la velocitat actual dels coets */
-showSpeed(r1, "speeds");
-showSpeed(r2, "speeds");
-/* Accelerar amb els coets tres cops */
-rocketAcceleration(r1, 3);
-rocketAcceleration(r2, 3);
-/* Mostrar a pantalla la velocitat actual */
-showSpeed(r1, "speeds");
-showSpeed(r2, "speeds");
-/* Frenar cinc cops amb el primer coet (“32WESSDS”) i accelerar set amb el segon coet. */
-rocketDeceleration(r1, 5);
-rocketAcceleration(r2, 7);
-/* Mostrar a pantalla la velocitat actual */
-showSpeed(r1, "speeds");
-showSpeed(r2, "speeds");
-/* Accelerar 15 cops amb els dos coets. */
-rocketAcceleration(r1, 15);
-rocketAcceleration(r2, 15);
-/* Mostrar a pantalla la velocitat actual */
-showSpeed(r1, "speeds");
-showSpeed(r2, "speeds");
+/*-- Crea el cohete pero no lo muestra por pantalla --*/
+function createRocketBtn(btn, defRocket) {
+    var r = createRocket(defRocket.id, defRocket.engines, defRocket.maxPowers);
+    btn.setAttribute("disabled", "");
+    return r;
+}
+/*-- devuelve el valor para el color del boton según si es el cohete 1 o 2 u otro --*/
+var str = function (n) {
+    var btn;
+    switch (n) {
+        case 1:
+            btn = "danger";
+            break;
+        case 2:
+            btn = "warning";
+            break;
+        default:
+            btn = "info";
+            break;
+    }
+    return btn;
+};
+/*-- devuelve el valores de HTML para crear el botón según el tipo pasádo al parámetro str --*/
+var insertHTML = function (str, n, btn) {
+    var speed;
+    var rocket;
+    str == "speed+" ? speed = "Ac" : speed = "De";
+    n == 0 ? rocket = "" : rocket = "rocket" + n + " ";
+    var HTMLstr = "<button class=\"" + rocket + "btn btn-" + btn + " ";
+    switch (str) {
+        case "speed+": /*-- // boton de acelerar // --*/
+        case "speed-": /*-- // boton de decelerar // --*/
+            HTMLstr += "mx-2\" id=\"" + speed + "celR" + n + "\">" + speed + "celerate Rocket " + n + "</button>";
+            break;
+        case "info": /*-- // boton de info de uno u otro cohete o ambos // --*/
+            if (n == 1 || n == 2) {
+                HTMLstr += "m-3\" id=\"infoR" + n + "\">Info Rocket " + n + "</button>";
+            }
+            else {
+                HTMLstr += "m-3\" id=\"infoRockets\">Info All Rockets</button>";
+            }
+            break;
+    }
+    return HTMLstr;
+};
+/*-- // creación de botones // --*/
+/*-- según el valor recibido crear uno u otro botón --*/
+function createBtn(btnType, elemId, n) {
+    var btn = str(n);
+    var section = document.getElementById(elemId);
+    var HTMLbtn = insertHTML(btnType, n, btn);
+    section.innerHTML += HTMLbtn;
+}
+var btnCreateR1 = document.getElementById("btnCreateR1");
+var btnCreateR2 = document.getElementById("btnCreateR2");
+/*-- crea el cohete y sus botones asociados --*/
+function addBtns(n) {
+    createBtn("info", "infoBtns", n);
+    if (btnCreateR1.hasAttribute("disabled") && btnCreateR2.hasAttribute("disabled")) {
+        createBtn("info", "infoBtns", 0);
+        //activa el evenlistener
+        infoBtn(0);
+    }
+    createBtn("speed+", "speedBtns", n);
+    createBtn("speed-", "speedBtns", n);
+}
+/*-- crea el cohete 1 y sus botones asociados --*/
+btnCreateR1.addEventListener("click", function () {
+    r1 = createRocketBtn(btnCreateR1, defaultRockets[0]);
+    addBtns(1);
+    //activa los eventlisteners
+    infoBtn(1);
+    accelBtn(1);
+    decelBtn(1);
+    if (r2 != undefined) {
+        infoBtn(2);
+        accelBtn(2);
+        decelBtn(2);
+    }
+});
+/*-- crea el cohete 2 y sus botones asociados --*/
+btnCreateR2.addEventListener("click", function () {
+    r2 = createRocketBtn(btnCreateR2, defaultRockets[1]);
+    addBtns(2);
+    //activa los eventlisteners
+    infoBtn(2);
+    accelBtn(2);
+    decelBtn(2);
+    if (r1 != undefined) {
+        infoBtn(1);
+        accelBtn(1);
+        decelBtn(1);
+    }
+});
+/*-- botones de info --*/
+function infoBtn(btn) {
+    var btnAllInfo = document.getElementById("infoRockets");
+    var btnInfoR1 = document.getElementById("infoR1");
+    var btnInfoR2 = document.getElementById("infoR2");
+    switch (btn) {
+        case 1: /*-- info Roket 1 --*/
+            InfoR1btn(btnInfoR1, btnInfoR2, btnAllInfo);
+            break;
+        case 2: /*-- info Roket 2 --*/
+            InfoR2btn(btnInfoR1, btnInfoR2, btnAllInfo);
+            break;
+        case 0: /*-- info All Rokets --*/
+            InfoAllBtn(btnInfoR1, btnInfoR2, btnAllInfo);
+            break;
+    }
+}
+/*-- al pulsar el botón muestra la info del cohete 1 y luego lo deshabilita
+* y, si se ha pulsado también el botón de info del cohete 2, deshabilita el boton de toda la info --*/
+function InfoR1btn(btnInfoR1, btnInfoR2, btnAllInfo) {
+    btnInfoR1.addEventListener("click", function () {
+        showRocket(r1, "rockets");
+        btnInfoR1.setAttribute("disabled", "");
+        if (btnInfoR2 != null && btnInfoR2.hasAttribute("disabled")) {
+            btnAllInfo.setAttribute("disabled", "");
+        }
+    });
+}
+/*-- al pulsar el botón muestra la info del cohete 2 y luego lo deshabilita
+* y, si se ha pulsado también el botón de info del cohete 1, deshabilita el boton de toda la info --*/
+function InfoR2btn(btnInfoR1, btnInfoR2, btnAllInfo) {
+    btnInfoR2.addEventListener("click", function () {
+        showRocket(r2, "rockets");
+        btnInfoR2.setAttribute("disabled", "");
+        if (btnInfoR1 != null && btnInfoR1.hasAttribute("disabled")) {
+            btnAllInfo.setAttribute("disabled", "");
+        }
+    });
+}
+/*-- muestra la info de ambos cohetes o de uno de ellos si se ha pulsado el botón correspondiente,
+* y luego deshabilita el/los botón/es correspondiente/s --*/
+function InfoAllBtn(btnInfoR1, btnInfoR2, btnAllInfo) {
+    btnAllInfo.addEventListener("click", function () {
+        if (!btnInfoR1.hasAttribute("disabled") && !btnInfoR2.hasAttribute("disabled")) {
+            showRocket(r1, "rockets");
+            showRocket(r2, "rockets");
+            btnInfoR1.setAttribute("disabled", "");
+            btnInfoR2.setAttribute("disabled", "");
+        }
+        else if (!btnInfoR1.hasAttribute("disabled") && btnInfoR2.hasAttribute("disabled")) {
+            showRocket(r1, "rockets");
+            btnInfoR1.setAttribute("disabled", "");
+        }
+        else if (btnInfoR1.hasAttribute("disabled") && !btnInfoR2.hasAttribute("disabled")) {
+            showRocket(r2, "rockets");
+            btnInfoR2.setAttribute("disabled", "");
+        }
+        btnAllInfo.setAttribute("disabled", "");
+    });
+}
+/*-- botones de acelerar --*/
+function accelBtn(n) {
+    var btnAccelR1 = document.getElementById("AccelR1");
+    var btnAccelR2 = document.getElementById("AccelR2");
+    switch (n) {
+        case 1:
+            accelR1Btn(btnAccelR1, btnAccelR2);
+            break;
+        case 2:
+            accelR2Btn(btnAccelR1, btnAccelR2);
+            break;
+    }
+}
+function accelR1Btn(btnAccelR1, btnAccelR2) {
+    btnAccelR1.addEventListener("click", function () {
+        rocketAcceleration(r1, 1);
+        showSpeed(r1, "speeds");
+    });
+}
+function accelR2Btn(btnAccelR1, btnAccelR2) {
+    btnAccelR2.addEventListener("click", function () {
+        rocketAcceleration(r2, 1);
+        showSpeed(r2, "speeds");
+    });
+}
+/*-- botones de decelerar --*/
+function decelBtn(n) {
+    var btnDecelR1 = document.getElementById("DecelR1");
+    var btnDecelR2 = document.getElementById("DecelR2");
+    switch (n) {
+        case 1:
+            decelR1Btn(btnDecelR1, btnDecelR2);
+            break;
+        case 2:
+            decelR2Btn(btnDecelR1, btnDecelR2);
+            break;
+    }
+}
+function decelR1Btn(btnDecelR1, btnDecelR2) {
+    btnDecelR1.addEventListener("click", function () {
+        rocketDeceleration(r1, 1);
+        showSpeed(r1, "speeds");
+    });
+}
+function decelR2Btn(btnDecelR1, btnDecelR2) {
+    btnDecelR2.addEventListener("click", function () {
+        rocketDeceleration(r2, 1);
+        showSpeed(r2, "speeds");
+    });
+}
--- Controllers/controller.ts
+++ Controllers/controller.ts
@@ -1,25 +1,35 @@
 /*-- Default Rockets --*/
-/* //Roquet 1// */
-let rId1 = "32WESSDS";
-let rEngine1 = 3;
-let rMaxPowers1 = [10, 30, 80];
-let r1 = <Rocket>createRocket(rId1,rEngine1, rMaxPowers1);
+interface defaultRocket {
+    id:string;
+    engines:number;
+    maxPowers:number[];
+}
 
-/* //Roquet 2// */
-let rId2 = "LDSFJA32";
-let rEngine2 = 6;
-let rMaxPowers2 = [30, 40, 50, 50, 30, 10];
-let r2 = <Rocket>createRocket(rId2,rEngine2, rMaxPowers2);
+let defaultRockets:defaultRocket[] = [
+    { /* //Rocket 1// */
+        id: "32WESSDS", 
+        engines: 3,
+        maxPowers: [10, 30, 80]    
+    },
+    { /* //Rocket 2// */
+        id: "LDSFJA32", 
+        engines: 6,
+        maxPowers:[30, 40, 50, 50, 30, 10]
+    }
+];
+
+/* //Rocket 1// */
+let r1:Rocket;
+
+/* //Rocket 2// */
+let r2:Rocket;
+
 
 /*-- Comprueba si la Id del cohete cumple el patrón --*/
 function idValidator(value:string) {
         let idFormat = /[A-Z0-9]{8}/; //comprueba que sean 8 carácteres alfanuméricos 
-        let idRegExp = new RegExp(idFormat);
-        if (idRegExp.test(value)){
-            return true;
-        } else {
-            return false; 
-        }
+        let idRegExp = new RegExp(idFormat);        
+        if (idRegExp.test(value)){ return true; } else { return false; }
     }
 
 /*-- tras comprobar que la Id es correcta, crea el cohete --*/
@@ -58,7 +68,7 @@ function showSpeed (rocket:Rocket, sectionId:string){
         rocketSpeed += engine.engineAtualSpeed;
     }
     let HTMLSpeed = `<p><b>${rocket.id}</b> Rocket speed <i>${rocketSpeed}</i><p>`;
-    section.innerHTML += HTMLSpeed;
+    section.innerHTML = HTMLSpeed;
 }
 
 /*-- llama a la función acelerar del cohete las veces que se indique --*/
@@ -75,38 +85,226 @@ function rocketDeceleration (rocket:Rocket, times:number){
     }
 }
 
-/*-- FIXME: pasos por pantalla --*/
-   
-/* Mostrar a pantalla el codi dels coets, el número de propulsors 
-    que té i la potència màxima de cada propulsor.*/
-showRocket(r1, "rockets");
-showRocket(r2, "rockets");
+/*-- Crea el cohete pero no lo muestra por pantalla --*/
+function createRocketBtn (btn:HTMLButtonElement, defRocket:defaultRocket){
+    let r = <Rocket>createRocket(defRocket.id, defRocket.engines, defRocket.maxPowers); 
+    btn.setAttribute("disabled", "");    
+    return r;
+}
 
-/* Mostrar a pantalla la velocitat actual dels coets */
-showSpeed(r1, "speeds");
-showSpeed(r2, "speeds");
+/*-- devuelve el valor para el color del boton según si es el cohete 1 o 2 u otro --*/
+let str = function (n:number):string {
+    let btn:string;
+    switch(n){
+        case 1:
+            btn = "danger";
+            break;  
+        case 2:
+            btn = "warning";
+            break;
+        default:
+            btn = "info";
+            break;
+    }
+    return btn;
+}
 
-/* Accelerar amb els coets tres cops */
-rocketAcceleration(r1, 3);
-rocketAcceleration(r2, 3);
+/*-- devuelve el valores de HTML para crear el botón según el tipo pasádo al parámetro str --*/
+let insertHTML = function (str:string, n:number, btn:string):string {
+    let speed:string;
+    let rocket:string;
+    str == "speed+"? speed = "Ac": speed = "De";
+    n == 0? rocket = "" : rocket = `rocket${n} `;
+    let HTMLstr:string = `<button class="${rocket}btn btn-${btn} `;    
+    switch(str){
+        case "speed+": /*-- // boton de acelerar // --*/            
+        case "speed-": /*-- // boton de decelerar // --*/            
+            HTMLstr += `mx-2" id="${speed}celR${n}">${speed}celerate Rocket ${n}</button>`;
+            break;
+        case "info": /*-- // boton de info de uno u otro cohete o ambos // --*/
+            if (n == 1 || n == 2) {
+                HTMLstr += `m-3" id="infoR${n}">Info Rocket ${n}</button>`;
+            } else {
+                HTMLstr += `m-3" id="infoRockets">Info All Rockets</button>`;
+            }
+            break;
+    }
+    return HTMLstr;
+}
 
-/* Mostrar a pantalla la velocitat actual */
-showSpeed(r1, "speeds");
-showSpeed(r2, "speeds");
+/*-- // creación de botones // --*/
 
-/* Frenar cinc cops amb el primer coet (“32WESSDS”) i accelerar set amb el segon coet. */
-rocketDeceleration(r1, 5);
+/*-- según el valor recibido crear uno u otro botón --*/
+function createBtn (btnType:string, elemId:string, n:number) {
+    let btn:string = str(n);
+    let section = <HTMLElement>document.getElementById(elemId);    
+    let HTMLbtn = insertHTML(btnType, n, btn);        
+    section.innerHTML += HTMLbtn;
+}
 
-rocketAcceleration(r2, 7);
+let btnCreateR1 = <HTMLButtonElement>document.getElementById("btnCreateR1");
+let btnCreateR2 = <HTMLButtonElement>document.getElementById("btnCreateR2");
 
-/* Mostrar a pantalla la velocitat actual */
-showSpeed(r1, "speeds");
-showSpeed(r2, "speeds");
+/*-- crea el cohete y sus botones asociados --*/
+function addBtns (n:number) {
+    createBtn("info", "infoBtns", n);
+    if (btnCreateR1.hasAttribute("disabled") && btnCreateR2.hasAttribute("disabled")) {
+        createBtn("info", "infoBtns", 0);
+        //activa el evenlistener
+        infoBtn(0);
+    }
+    createBtn("speed+", "speedBtns", n);
+    createBtn("speed-", "speedBtns", n);
+}
 
-/* Accelerar 15 cops amb els dos coets. */
-rocketAcceleration(r1, 15);
-rocketAcceleration(r2, 15);
+/*-- crea el cohete 1 y sus botones asociados --*/
+btnCreateR1.addEventListener("click", () => {
+    r1 = createRocketBtn(btnCreateR1, defaultRockets[0]);    
+    addBtns(1);
+    //activa los eventlisteners
+    infoBtn(1);
+    accelBtn(1);
+    decelBtn(1);
+    if (r2 != undefined){ 
+        infoBtn(2);
+        accelBtn(2);
+        decelBtn(2);
+    }
+    
+});
 
-/* Mostrar a pantalla la velocitat actual */
-showSpeed(r1, "speeds");
-showSpeed(r2, "speeds");
+/*-- crea el cohete 2 y sus botones asociados --*/
+btnCreateR2.addEventListener("click", () => {
+    r2 = createRocketBtn(btnCreateR2, defaultRockets[1]);
+    addBtns(2);
+    //activa los eventlisteners
+    infoBtn(2);
+    accelBtn(2);
+    decelBtn(2);
+    if (r1 != undefined){
+        infoBtn(1);
+        accelBtn(1);
+        decelBtn(1);
+    }
+});
+
+/*-- botones de info --*/
+
+function infoBtn (btn:number) {
+    let btnAllInfo = <HTMLButtonElement>document.getElementById("infoRockets");
+    let btnInfoR1 = <HTMLButtonElement>document.getElementById("infoR1");
+    let btnInfoR2 = <HTMLButtonElement>document.getElementById("infoR2");
+    switch (btn) {
+        case 1: /*-- info Roket 1 --*/
+            InfoR1btn(btnInfoR1, btnInfoR2, btnAllInfo);
+            break;
+        case 2: /*-- info Roket 2 --*/
+            InfoR2btn(btnInfoR1, btnInfoR2, btnAllInfo);
+            break;
+        case 0: /*-- info All Rokets --*/
+            InfoAllBtn(btnInfoR1, btnInfoR2, btnAllInfo)
+            break;
+    }
+}
+
+/*-- al pulsar el botón muestra la info del cohete 1 y luego lo deshabilita 
+* y, si se ha pulsado también el botón de info del cohete 2, deshabilita el boton de toda la info --*/ 
+function InfoR1btn (btnInfoR1:HTMLButtonElement, btnInfoR2:HTMLButtonElement, btnAllInfo:HTMLButtonElement) {
+    btnInfoR1.addEventListener("click", () => {
+        showRocket(r1, "rockets");
+        btnInfoR1.setAttribute("disabled", "");
+        if (btnInfoR2 != null && btnInfoR2.hasAttribute("disabled")){
+            btnAllInfo.setAttribute("disabled", "");
+        }
+    });    
+}
+
+/*-- al pulsar el botón muestra la info del cohete 2 y luego lo deshabilita 
+* y, si se ha pulsado también el botón de info del cohete 1, deshabilita el boton de toda la info --*/
+function InfoR2btn (btnInfoR1:HTMLButtonElement, btnInfoR2:HTMLButtonElement, btnAllInfo:HTMLButtonElement) {
+    btnInfoR2.addEventListener("click", () => {
+        showRocket(r2, "rockets");
+        btnInfoR2.setAttribute("disabled", "");
+        if (btnInfoR1 != null && btnInfoR1.hasAttribute("disabled")){
+            btnAllInfo.setAttribute("disabled", "");
+        }
+    });    
+}
+
+/*-- muestra la info de ambos cohetes o de uno de ellos si se ha pulsado el botón correspondiente,
+* y luego deshabilita el/los botón/es correspondiente/s --*/
+function InfoAllBtn (btnInfoR1:HTMLButtonElement, btnInfoR2:HTMLButtonElement, btnAllInfo:HTMLButtonElement) {
+    btnAllInfo.addEventListener("click", () => {
+        if (!btnInfoR1.hasAttribute("disabled") && !btnInfoR2.hasAttribute("disabled")){
+            showRocket(r1, "rockets");
+            showRocket(r2, "rockets");
+            btnInfoR1.setAttribute("disabled", "");
+            btnInfoR2.setAttribute("disabled", "");
+        } else if (!btnInfoR1.hasAttribute("disabled") && btnInfoR2.hasAttribute("disabled")){
+            showRocket(r1, "rockets");
+            btnInfoR1.setAttribute("disabled", "");
+        } else if (btnInfoR1.hasAttribute("disabled") && !btnInfoR2.hasAttribute("disabled")){
+            showRocket(r2, "rockets");
+            btnInfoR2.setAttribute("disabled", "");
+        }
+        btnAllInfo.setAttribute("disabled", "");
+    });    
+}
+
+/*-- botones de acelerar --*/
+
+function accelBtn (n:number){
+    let btnAccelR1 = <HTMLButtonElement>document.getElementById("AccelR1");
+    let btnAccelR2 = <HTMLButtonElement>document.getElementById("AccelR2");
+    switch(n){
+        case 1:
+            accelR1Btn (btnAccelR1, btnAccelR2);
+            break;
+        case 2:
+            accelR2Btn (btnAccelR1, btnAccelR2);
+            break;            
+    }
+}
+
+function accelR1Btn (btnAccelR1:HTMLButtonElement, btnAccelR2:HTMLButtonElement) {
+    btnAccelR1.addEventListener ("click", () => {
+        rocketAcceleration(r1, 1);
+        showSpeed(r1, "speeds");
+    });
+}
+
+function accelR2Btn (btnAccelR1:HTMLButtonElement, btnAccelR2:HTMLButtonElement) {
+    btnAccelR2.addEventListener ("click", () => {
+        rocketAcceleration(r2, 1);
+        showSpeed(r2, "speeds");
+    });
+}
+
+/*-- botones de decelerar --*/
+
+function decelBtn (n:number){
+    let btnDecelR1 = <HTMLButtonElement>document.getElementById("DecelR1");
+    let btnDecelR2 = <HTMLButtonElement>document.getElementById("DecelR2");
+    switch(n){
+        case 1:
+            decelR1Btn (btnDecelR1, btnDecelR2);
+            break;
+        case 2:
+            decelR2Btn (btnDecelR1, btnDecelR2);
+            break;            
+    }
+}
+
+function decelR1Btn (btnDecelR1:HTMLButtonElement, btnDecelR2:HTMLButtonElement) {
+    btnDecelR1.addEventListener ("click", () => {
+        rocketDeceleration(r1, 1);
+        showSpeed(r1, "speeds");
+    });
+}
+
+function decelR2Btn (btnDecelR1:HTMLButtonElement, btnDecelR2:HTMLButtonElement) {
+    btnDecelR2.addEventListener ("click", () => {
+        rocketDeceleration(r2, 1);
+        showSpeed(r2, "speeds");
+    });
+}
